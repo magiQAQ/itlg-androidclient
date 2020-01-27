deb3e5ee5172fe754c207b0883a939f1fbcbd422
Seven Deadly Sins <xchxch1996@gmail.com>
2020-01-27 22:16:42 +0800

    添加签名

---
+++ app/src/androidTest/java/com/itlg/client/ExampleInstrumentedTest.java
@@ -0,0 +1,15 @@
+package com.itlg.client;
+
+
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    public void test() {
+
+    }
+
+}
